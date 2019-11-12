4118f375b1a3fba2a5eac396c2b78c8c28876679
yunine16 <yuchin616@icloud.com>
2019-11-12 21:45:27 +0900

    リスタート

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-start">
+      <img src="#" alt="jackのアイコン">
+    </div>
+    <div class="navbar-end">
+      <p class="navbar-item"><a>Home</a></p>
+      <p class="navbar-item"><a>Works</a></p>
+      <p class="navbar-item"><a>Contacts</a></p>
+    </div>
+  </nav>
+</template>
+
+<style>
+.navbar{
+  background-color: #FDBE46;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div id="about_us" class="contents">
+      aboutus
+    </div>
+    <contact />
+  </div>
+</template>
+
+<script>
+import Contact from '~/components/contact.vue'
+
+export default {
+  components: {
+    Contact
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
