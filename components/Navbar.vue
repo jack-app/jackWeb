9ad0de52490c1cd1d36f1278cc9d6b46df8b76bd
yunine16 <yuchin616@icloud.com>
2019-11-30 16:18:12 +0900

    masterに追いつく

---
+++ components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="navbar-brand">
+      <div class="jack-icon">
+        <a href="#home"><img src="~/static/jack-icon.png" alt="jackのアイコン"></a>
+      </div>
+      <div class="navbar-burger burger" data-target="navMenu" @click="$store.commit('toggleMenu')" :class="{'is-active': $store.state.isMenuActive}">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div class="navbar-menu" id="navMenu" :class="{'is-active':$store.state.isMenuActive}">
+      <div class="navbar-end">
+        <p class="navbar-item"><a href="#home">Home</a></p>
+        <p class="navbar-item"><a href="#about">About us</a></p>
+        <p class="navbar-item"><a href="#works">Works</a></p>
+        <p class="navbar-item"><a href="#contact">Contact</a></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  fetch({store}) {
+    store.commit('resetMenu')
+  }
+}
+</script>
+
+<style scoped>
+.container{
+  padding: 0 50px;
+}
+</style>
